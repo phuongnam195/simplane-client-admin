091e8fbaa5eaed7b4d4327f4ae04acf02b80f29c
Phuong Nam <phuongnam1952001@gmail.com>
2022-06-22 09:15:28 +0700

    Rule: load airport (dummy)

--- lib/core/rule_manager.dart
+++ lib/core/rule_manager.dart
@@ -1,3 +1,4 @@
+import 'package:simplane_client_admin/model/airport.dart';
 import 'package:simplane_client_admin/model/ticket_class.dart';
 import 'package:simplane_client_admin/network/api_path.dart';
 
@@ -7,9 +8,14 @@ class RuleManager {
   static final RuleManager instance = RuleManager._internal();
   RuleManager._internal();
 
+  List<Airport> _listAirport = [];
   List<TicketClass> _listTicketClass = [];
 
+  List<Airport> getListAirport() => _listAirport;
   List<TicketClass> getListTicketClass() => _listTicketClass;
+
+  Airport? getAirport(String code) =>
+      _listAirport.firstWhere((e) => e.code == code);
 
   TicketClass? getTicketClass(String id) =>
       _listTicketClass.firstWhere((e) => e.id == id);
@@ -17,6 +23,7 @@ class RuleManager {
   Future<void> load() async {
     // TODO: chưa có API
     // _listTicketClass = TicketClass.mapToList(await getListFromApi(apiUrl: TICKET_CLASS_GETS));
+    _listAirport = airportsDummy;
     _listTicketClass = ticketClassesDummy;
   }
 }
